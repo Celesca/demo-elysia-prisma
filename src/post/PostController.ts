369ab332f1822624fb61625e67be72f28adf6b45
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-27 00:52:01 +0700

    ♻️ add: post patch delete

--- src/post/PostController.ts
+++ src/post/PostController.ts
@@ -1,38 +1,37 @@
-import { Elysia, t } from 'elysia';
+import { Elysia, t } from "elysia";
 import {
-    createPost,
-    getPost,
-    getPosts,
-    updatePost,
-    deletePost
-} from './PostHandlers';
+  createPost,
+  getPost,
+  getPosts,
+  updatePost,
+  deletePost,
+} from "./PostHandlers";
 
-const postController = new Elysia({ prefix: '/posts'})
-    .model({
-        'post.body': t.Object({
-            title: t.String()
-        }),
-        'post.id': t.Object({
-            id: t.Numeric()
-        })
+const postController = new Elysia({ prefix: "/posts" })
+  .model({
+    "post.body": t.Object({
+      title: t.String(),
+      content: t.String(),
+    }),
+    "post.id": t.Object({
+      id: t.Numeric(),
+    }),
+  })
+  .get("/", () => getPosts())
+  .get("/:id", ({ params: { id } }) => getPost(id), {
+    params: "post.id",
+  })
+  .post("/", ({ body }) => createPost(body), {
+    body: "post.body",
+  })
+  .patch("/:id", ({ params: { id }, body }) => updatePost(id, body), {
+    params: "post.id",
+    body: "post.body",
+  })
+  .delete("/", ({ body }) => deletePost(body), {
+    body: t.Object({
+        id: t.Numeric()
     })
-    .get('/', () => getPosts())
-    .get('/:id', 
-    ({ params: { id } }) => getPost(id), {
-        params: t.Object({
-            id: t.Numeric()
-        })
-    })
-    .post('/', ({ body }) => createPost(body), {
-        body: 'post.body'
-    })
-    .patch('/:id', ({ params : { id } , body}) => updatePost(id, body), {
-        params: t.Object({
-            id: t.Numeric()
-        }),
-        body: 'post.body'
-    }
-    )
-    .delete('/', () => 'delete post');
+  });
 
 export default postController;
